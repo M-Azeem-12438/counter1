0622ae3eae5cf23c202f05a9e97d3e747c2f699d
M-Azeem-12438 <azeemhaleem1947@gmail.com>
2021-03-11 20:31:18 -0800

    counter

---
+++ count.js
@@ -0,0 +1,11 @@
+var count=0
+function count1()
+{
+count=count+1;
+document.getElementById("count").innerHTML=count;
+}
+function reset()
+{
+    count=0
+    document.getElementById("count").innerHTML=count;
+}
